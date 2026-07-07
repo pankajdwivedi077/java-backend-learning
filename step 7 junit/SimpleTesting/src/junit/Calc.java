3f5e301aaab34d741fc006b8905ae256bd7b09b6
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2026-07-07 19:46:15 +0530

    gradle

---
+++ step 7 junit/SimpleTesting/src/junit/Calc.java
@@ -0,0 +1,13 @@
+package junit;
+
+public class Calc {
+    
+    // one unit
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+    
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
---
+++ step 7 junit/SimpleTesting/src/junit/Main.java
@@ -0,0 +1,8 @@
+package junit;
+
+public class Main {
+   public static void main(String[] args) {
+	   Calc cal = new Calc();
+	   int result = cal.divide(10, 5);
+   }
+}
